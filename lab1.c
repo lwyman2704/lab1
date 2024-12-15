0f3b48e68cf7ad2be6bd20adffc8c5f9959c2dda
lwyman2704 <153244844+lwyman2704@users.noreply.github.com>
2024-12-15 01:03:54 -0500

    Add files via upload

---
+++ lab1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+    int age;
+
+    // User Input:
+
+        printf("Enter your age: ");
+        scanf("%d",&age);
+        printf("you are %d years old.", age);
+    return 0;
+    
+
+}
